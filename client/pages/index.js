bdffdd88d0f6a3bcedb60da061eab38d22cc145f
Michael Lally <mikelally801@gmail.com>
2024-02-26 15:22:19 -0500

    updated console log for API call

--- client/pages/index.js
+++ client/pages/index.js
@@ -4,7 +4,7 @@ function Index() {
   
   const [message, setMessage] = useState("Loading")
 
-  console.log(process.env.NEXT_PUBLIC_SERVER_URL)
+  console.log(process.env.NEXT_PUBLIC_SERVER_URL + "/api/home")
   useEffect(() => {
     fetch(process.env.NEXT_PUBLIC_SERVER_URL + "/api/home").then(
       response => response.json()
