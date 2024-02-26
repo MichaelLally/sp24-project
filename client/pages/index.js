8c8b2b0b1974e215e983a27a5ce07a2c6e7c9f90
Michael Lally <mikelally801@gmail.com>
2024-02-26 14:09:11 -0500

    added process.env for server url on client, updated api message on server

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,9 +3,15 @@ import React, {useEffect, useState} from 'react'
 function Index() {
   
   const [message, setMessage] = useState("Loading")
-  
+
+  if(process.env.SERVER_URL) {
+    const url = process.env.SERVER_URL // prod
+  } else {
+    const url = "http://localhost:8080" // local
+  }
+
   useEffect(() => {
-    fetch("http://localhost:8080/api/home").then(
+    fetch(url + "/api/home").then(
       response => response.json()
     ).then(
       data => {
